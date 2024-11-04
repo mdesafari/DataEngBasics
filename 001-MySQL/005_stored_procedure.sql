3b0d4f44bc030258f9178c9623091bb82a452f8c
avadombi <vp.adombi@gmail.com>
2024-11-04 11:07:53 -0500

    Create stored procedure and views

---
+++ 001-MySQL/005_stored_procedure.sql
@@ -0,0 +1,25 @@
+-- use our db;
+USE country_db;
+
+-- create stored procedure
+DROP PROCEDURE IF EXISTS display_table;
+CREATE PROCEDURE display_table(IN chosenTable VARCHAR(15))
+BEGIN
+    -- use a case
+    CASE chosenTable
+        WHEN 'Countries' THEN
+            SELECT country_name, region FROM countries
+            ORDER BY country_name ASC;
+        
+        WHEN 'Cities' THEN
+            SELECT city_name, FORMAT(population, 0) FROM cities
+            ORDER BY city_name ASC;
+        
+        ELSE
+            SELECT * FROM currencies;
+    END CASE;
+END;
+
+
+-- use the stored procedure
+CALL display_table('Countries');
